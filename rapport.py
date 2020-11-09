3a9438222d785add44773800198bec3ffdd38603
Thomas GEORGE <56248590+tpcg2@users.noreply.github.com>
2020-11-09 18:03:53 +0100

    Update rapport.py

--- rapport.py
+++ rapport.py
@@ -326,7 +326,7 @@ Here we use a subset of the test data to try and find some miss classification.
 It will help us understand why the neural network failed sometimes to classify images.
 """
 
-is _name_ == "_main_":
+if _name_ == "_main_":
   nsample = 1000
   X_demo = X_test[:nsample,:]
   y_demo = ffnn.forward_pass(X_demo)
